f5002093ee1a4950812101b71fcd30d52158ee1b
stenet <stefan.heim@hotmail.com>
2022-11-22 20:16:56 +0100

    feat: tab content

--- src/App.vue
+++ src/App.vue
@@ -7,16 +7,49 @@ import MyTabItem from './components/MyTabItem.vue';
   <div class="p-4">
     <MyTab>
       <MyTabItem text="Page 1">
-        <div>Inhalt</div>
+        <div>Content of Page 1 😉</div>
       </MyTabItem>
       <MyTabItem text="Page 2">
-        <div>sdfasdfawef</div>
+        <div>Content of Page 2 👏</div>
       </MyTabItem>
       <MyTabItem text="Page 3">
-        <div>Idcvyxvxcvdfbnhalt</div>
+        <div>Content of Page 3 🚀</div>
       </MyTabItem>
       <MyTabItem text="Page 4">
-        <div>Inhdbfbebea3alt</div>
+        <div>Content of Page 4 🇪🇸</div>
+      </MyTabItem>
+      <MyTabItem text="Page 5">
+        <div>Content of Page 5 🇨🇭</div>
+      </MyTabItem>
+      <MyTabItem text="Page 6">
+        <div>Content of Page 6 😆</div>
+      </MyTabItem>
+      <MyTabItem text="Page 7">
+        <div>Content of Page 7 😇</div>
+      </MyTabItem>
+      <MyTabItem text="Page 8">
+        <div>Content of Page 8 😬</div>
+      </MyTabItem>
+      <MyTabItem text="Page 9">
+        <div>Content of Page 9 🧐</div>
+      </MyTabItem>
+      <MyTabItem text="Page 10">
+        <div>Content of Page 10 😝</div>
+      </MyTabItem>
+      <MyTabItem text="Page 11">
+        <div>Content of Page 11 🫥</div>
+      </MyTabItem>
+      <MyTabItem text="Page 12">
+        <div>Content of Page 12 🤬</div>
+      </MyTabItem>
+      <MyTabItem text="Page 13">
+        <div>Content of Page 13 👹</div>
+      </MyTabItem>
+      <MyTabItem text="Page 14">
+        <div>Content of Page 14 😈</div>
+      </MyTabItem>
+      <MyTabItem text="Page 15">
+        <div>Content of Page 15 🤢</div>
       </MyTabItem>
     </MyTab>
   </div>
--- src/components/MyTab.vue
+++ src/components/MyTab.vue
@@ -1,13 +1,23 @@
 <script lang="ts">
-import { h, ref } from 'vue';
+import { h, ref, onMounted, onUnmounted } from 'vue';
 
 export default {
   setup(props, context) {
     const selected = ref(0);
     const itemRefs = ref({});
     const tabRef = ref();
+    const windowWidth = ref();
 
-    //TODO, wenn sich die Breite ändert, dann muss neu gerendert werden
+    const checkWindowResize = () => {
+      windowWidth.value = window.innerWidth;
+    };
+
+    onMounted(() => {
+      window.addEventListener('resize', checkWindowResize);
+    });
+    onUnmounted(() => {
+      window.removeEventListener('resize', checkWindowResize);
+    });
 
     return () => {
       const item =
@@ -23,51 +33,69 @@ export default {
 
       const itemRect = itemRects?.length > 0 ? itemRects[0] : null;
 
-      return h(
-        'div',
-        {
-          ref: (el) => (tabRef.value = el),
-          class:
-            'relative inline-flex flex-wrap gap-2 bg-neutral-300 rounded p-1',
-        },
-        {
-          default: () => [
-            ...context.slots.default?.().map((r, i) =>
-              h(
-                'div',
-                {
-                  ref: (el) => (itemRefs.value[i] = el),
-                  class: 'z-10',
-                  onClick: () => {
-                    selected.value = i;
-                  },
-                },
+      return [
+        h(
+          'div',
+          {
+            ref: (el) => (tabRef.value = el),
+            class:
+              'relative inline-flex flex-wrap gap-2 bg-neutral-300 rounded p-1',
+          },
+          {
+            default: () => [
+              ...context.slots.default?.().map((r, i) =>
                 h(
                   'div',
                   {
-                    class: 'px-2 py-1 text-sm rounded cursor-pointer',
+                    ref: (el) => (itemRefs.value[i] = el),
+                    class:
+                      'z-10 outline-neutral-600 rounded outline-neutral-300 hover:bg-neutral-100',
+                    tabIndex: 0,
+                    onClick: () => {
+                      selected.value = i;
+                    },
+                    onKeypress: (ev) => {
+                      console.log('keypress');
+                      if ([13, 32].includes(ev.keyCode)) {
+                        selected.value = i;
+                      }
+                    },
                   },
-                  r.props.text
+                  h(
+                    'div',
+                    {
+                      class: 'px-2 py-1 text-sm rounded cursor-pointer',
+                    },
+                    r.props.text
+                  )
                 )
-              )
-            ),
-            itemRect && tabRect
-              ? h(
-                  'div',
-                  {
-                    class: 'absolute rounded bg-white transition-all shadow',
-                    style: `left: ${itemRect.left - tabRect.left}px; top: ${
-                      itemRect.top - tabRect.top
-                    }px; width: ${itemRect.width}px; height: ${
-                      itemRect.height
-                    }px`,
-                  },
-                  ''
-                )
-              : null,
-          ],
-        }
-      );
+              ),
+              itemRect && tabRect
+                ? h(
+                    'div',
+                    {
+                      class: 'absolute rounded bg-white transition-all shadow',
+                      style: `left: ${itemRect.left - tabRect.left}px; top: ${
+                        itemRect.top - tabRect.top
+                      }px; width: ${itemRect.width}px; height: ${
+                        itemRect.height
+                      }px`,
+                      'data-w': windowWidth.value,
+                    },
+                    ''
+                  )
+                : null,
+            ],
+          }
+        ),
+        h(
+          'div',
+          {
+            class: 'mt-4',
+          },
+          context.slots.default?.().filter((r, i) => i == selected.value)
+        ),
+      ];
     };
   },
 };
